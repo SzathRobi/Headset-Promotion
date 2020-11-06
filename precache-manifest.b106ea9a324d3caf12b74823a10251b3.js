c37011652274135fd30393059570d8839d6bacfc
DevVatu <devvatu@gmail.com>
2020-11-06 15:52:04 +0100

    Updates

---
+++ precache-manifest.b106ea9a324d3caf12b74823a10251b3.js
@@ -0,0 +1,98 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0d9f0141c72fbd5818916bb7fb1f48a4",
+    "url": "/Headset-Promotion/index.html"
+  },
+  {
+    "revision": "4dab23d0bf672c02d6c1",
+    "url": "/Headset-Promotion/static/css/main.fce41adc.chunk.css"
+  },
+  {
+    "revision": "83bc20da0bb70a09b269",
+    "url": "/Headset-Promotion/static/js/2.744f94cb.chunk.js"
+  },
+  {
+    "revision": "65075b20798fa24012a1ad453c78c90d",
+    "url": "/Headset-Promotion/static/js/2.744f94cb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4dab23d0bf672c02d6c1",
+    "url": "/Headset-Promotion/static/js/main.93229426.chunk.js"
+  },
+  {
+    "revision": "fc925b0b2182162fc758",
+    "url": "/Headset-Promotion/static/js/runtime-main.9d63ad06.js"
+  },
+  {
+    "revision": "a275da664e6773e76cc43c8a075dbfd5",
+    "url": "/Headset-Promotion/static/media/about.a275da66.png"
+  },
+  {
+    "revision": "388748cbb0f87dc49c0d38b34068b64f",
+    "url": "/Headset-Promotion/static/media/aboutBg.388748cb.jpg"
+  },
+  {
+    "revision": "e6421101ccd2faa6979f87034a79b19e",
+    "url": "/Headset-Promotion/static/media/aboutSlider1.e6421101.png"
+  },
+  {
+    "revision": "3d39d5fc7585bf2d363b9320d83cf191",
+    "url": "/Headset-Promotion/static/media/aboutSlider2.3d39d5fc.png"
+  },
+  {
+    "revision": "3db6e93a2047b809d7c1376998d4766e",
+    "url": "/Headset-Promotion/static/media/aboutSlider3.3db6e93a.png"
+  },
+  {
+    "revision": "a275da664e6773e76cc43c8a075dbfd5",
+    "url": "/Headset-Promotion/static/media/faqs.a275da66.png"
+  },
+  {
+    "revision": "4220c41464357ad8ecf2c78fb061e5b4",
+    "url": "/Headset-Promotion/static/media/footer-bg.4220c414.png"
+  },
+  {
+    "revision": "34a353e8ca59dc6eb6a86b1ff0961fd1",
+    "url": "/Headset-Promotion/static/media/headset.34a353e8.png"
+  },
+  {
+    "revision": "9e5ca7b7f623f1a4f0e69b90c8076ae1",
+    "url": "/Headset-Promotion/static/media/headset_black.9e5ca7b7.png"
+  },
+  {
+    "revision": "3d39d5fc7585bf2d363b9320d83cf191",
+    "url": "/Headset-Promotion/static/media/headset_blue.3d39d5fc.png"
+  },
+  {
+    "revision": "054b4e2ad638ef72343598441e7fb86d",
+    "url": "/Headset-Promotion/static/media/headset_pink.054b4e2a.png"
+  },
+  {
+    "revision": "3db6e93a2047b809d7c1376998d4766e",
+    "url": "/Headset-Promotion/static/media/headset_red.3db6e93a.png"
+  },
+  {
+    "revision": "e6421101ccd2faa6979f87034a79b19e",
+    "url": "/Headset-Promotion/static/media/headset_white.e6421101.png"
+  },
+  {
+    "revision": "9cd3fd62cbf15bc34f28ebf5e4927594",
+    "url": "/Headset-Promotion/static/media/map.9cd3fd62.png"
+  },
+  {
+    "revision": "b076301ce96992b72db2ba5815ac9edf",
+    "url": "/Headset-Promotion/static/media/person-1.b076301c.jpg"
+  },
+  {
+    "revision": "a49007ddcc221342cb4fc09f9f0b9288",
+    "url": "/Headset-Promotion/static/media/person-3.a49007dd.jpg"
+  },
+  {
+    "revision": "50088aef0540111391d1848a41da9aec",
+    "url": "/Headset-Promotion/static/media/person-4.50088aef.jpg"
+  },
+  {
+    "revision": "49bad57cda3672176609aefd762b4847",
+    "url": "/Headset-Promotion/static/media/person-5.49bad57c.jpg"
+  }
+]);
